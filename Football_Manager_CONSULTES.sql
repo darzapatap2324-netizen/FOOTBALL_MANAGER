8c3bc997017ddc04630beb66ab92e019b5861a23
Darrell <darzapatap2324@politecnics.barcelona>
2026-02-23 13:46:48 +0100

    Consultas hasta la 4 hecha

--- Football_Manager_CONSULTES.sql
+++ Football_Manager_CONSULTES.sql
@@ -21,27 +21,38 @@ WHERE ciutats.nom IN  ('Barcelona', 'Madrid', 'Sevilla')
 AND persones.nom NOT LIKE 'F%'  AND persones.cognoms LIKE '%e%'; 
 
 -- 3r Consulta
+SELECT /*equips.id,*/ equips.nom, SUM(punts) AS 'Puntuacio_Total'
+FROM (
+	SELECT partits.equips_id_local AS 'equips_id', partits.punts_local AS 'punts'
+    FROM partits
+    JOIN equips ON equips.id = partits.equips_id_local AND partits.equips_id_visitant
+    JOIN participar_lligues ON participar_lligues.equips_id = equips.id
+	JOIN lligues ON  lligues.id = participar_lligues.lligues_id
+	WHERE lligues.nom = 'La Liga EA Sports' AND lligues.temporada = 2024
+    
+    UNION ALL
+    
+    SELECT partits.equips_id_visitant AS 'equips_id', partits.punts_visitant AS 'punts'
+    FROM partits
+    JOIN equips ON equips.id = partits.equips_id_local AND partits.equips_id_visitant
+    JOIN participar_lligues ON participar_lligues.equips_id = equips.id
+	JOIN lligues ON  lligues.id = participar_lligues.lligues_id
+	WHERE lligues.nom = 'La Liga EA Sports' AND lligues.temporada = 2024
+    ) AS equipos
+JOIN equips ON equips.id = equipos.equips_id
+GROUP BY equips.nom/*, equips.id*/
+ORDER BY Puntuacio_Total DESC;
+
+
+-- 4t Consulta
 USE football_manager;
-SELECT equips.nom, SUM(partits.punts_local + partits.punts_visitant) AS 'Puntuacio_Total'
-FROM equips
-JOIN partits ON partits.equips_id_local = equips.id 
-JOIN partits ON partits.equips_id_visitant = equips.id
-JOIN participar_lligues ON participar_lligues.equips_id = equips.id
-JOIN lligues ON  lligues.id = participar_lligues.lligues_id
-GROUP BY equips.nom
-WHERE lligues.nom = 'La Liga EA Sports' AND lligues.temporada = 2024
-ORDER BY 'Puntuacio Total' DESC;
-
-
-
-
-
-
-
-
-
-
-
+SELECT equips.nom, persones.tipus_persona, CONCAT(persones.nom, ' ' ,persones.cognoms) AS 'Nombre_Completo'
+FROM persones
+JOIN entrenadors ON entrenadors.persones_id = persones.id
+JOIN jugadors ON jugadors.persones_id = persones.id
+JOIN entrenar_equips ON entrenar_equips.entrenadors_id = persones.id AND entrenar_equips.equips_id = equips.id
+JOIN jugadors_equips ON jugadors_equips.jugadors_id = jugadors.persones_id AND jugadors_equips.equips_id = equips.id
+GROUP BY equips.nom;
 
 
 
