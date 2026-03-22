5d85c1ac036ce988af6e2a0ac653bd92cca59ab2
matias699 <mgomezt2324@politecnics.barcelona>
2026-03-22 06:22:58 +0100

    Update Football_Manager_Modificacio.sql

--- Football_Manager_Modificacio.sql
+++ Football_Manager_Modificacio.sql
@@ -1,3 +1,4 @@
+
 -- Copia de Seguretat
 create database if not exists football_manager_backup;
 create table football_manager_backup.ciutats_backup as select * from football_manager.ciutats;
@@ -15,20 +16,19 @@ create table football_manager_backup.partits_gols_backup as select * from footba
 create table football_manager_backup.persones_backup as select * from football_manager.persones;
 create table football_manager_backup.posicions_backup as select * from football_manager.posicions;
 
-
-
 -- Modificacions de DADES = Transaccions
 
 -- 1r Consulta Modificat
 /*Donat el nom d'una lliga i la temporada. Fer que tots els partits de la jornada 5 acabin en 0-0.*/
 START TRANSACTION;
-UPDATE football_manager.partits AS partitsback
-JOIN football_manager.equips AS equips_local ON equips_local.id = partitsback.equips_id
-JOIN football_manager.equips AS equips_visitant ON equips_visitant.id = partitsback.equips_id
-JOIN football_manager.jornades AS jornades ON jornades.id = partitsback.jornades_id
-JOIN football_manager.lligues AS lligues ON lligues.id = jornades.lligues_id
-SET gols_local = 0, gols_visitant = 0
-WHERE partitsback.jornades_id = 5 AND lligues.nom = 'La Liga EA Sports' AND lligues.temporada = 2024;
+UPDATE partits
+JOIN jornades ON jornades.id = partits.jornades_id
+JOIN lligues ON lligues.id = jornades.lligues_id
+SET partits.gols_local = 0, partits.gols_visitant = 0, partits.punts_local = 1, partits.punts_visitant = 1
+WHERE jornades.jornada = 5 
+AND lligues.nom = 'La Liga EA Sports' 
+AND lligues.temporada = 2024;
+COMMIT;
 /*ROLLBACK;*/
 /*COMMIT;*/
 
@@ -36,8 +36,9 @@ WHERE partitsback.jornades_id = 5 AND lligues.nom = 'La Liga EA Sports' AND llig
 /*La nostra base de dades està creixent molt i hem d'alliberar
  una mica d'espai. Esborra totes les ciutats en les quals no hi hagi cap equip.*/
 START TRANSACTION;
-DELETE FROM football_manager.ciutats AS ciutatsback
-WHERE ciutatsback.id NOT IN (SELECT DISTINCT ciutats_id FROM football_manager.equips WHERE ciutats_id IS NOT NULL);
+DELETE FROM ciutats
+WHERE ciutats.id NOT IN (SELECT DISTINCT ciutats_id FROM equips WHERE ciutats_id IS NOT NULL);
+COMMIT;
 /*ROLLBACK;*/
 /*COMMIT;*/
 
@@ -45,10 +46,12 @@ WHERE ciutatsback.id NOT IN (SELECT DISTINCT ciutats_id FROM football_manager.eq
 -- 3ra Consulta Modificat
 /*Baixa un 10% la motivació d'aquells jugadors que cobren menys de 5 milions i tenen 35 anys o més.*/
 START TRANSACTION;
-UPDATE football_manager.persones AS personesback
-JOIN football_manager.jugadors AS jugadorsback ON jugadorsback.persones_id = personesback.id
-SET personesback.sou = personesback.sou * 0.9
-WHERE personesback.sou > 5000000 AND TIMESTAMPDIFF(YEAR, personesback.data_naixement, CURTDATE()) >= 35;
+UPDATE persones
+JOIN jugadors ON jugadors.persones_id = persones.id
+SET persones.nivell_motivacio = persones.nivell_motivacio * 0.9
+WHERE persones.sou < 5000000 
+AND (YEAR(CURDATE()) - YEAR(persones.data_naixement)) >= 35;
+COMMIT;
 /*ROLLBACK;*/
 /*COMMIT;*/
 
@@ -57,15 +60,14 @@ WHERE personesback.sou > 5000000 AND TIMESTAMPDIFF(YEAR, personesback.data_naixe
 /*S'ha celebrat un entrenament especial de porters dels primers equips. Puja en 3 punts la
  qualitat de tots els porters de la base de dades que no pertanyin a un equip filial.*/
 START TRANSACTION;
-UPDATE football_manager.jugadors AS jugadorsback
-JOIN football_manager.jugadors_equips AS jugadors_equipsback ON jugadors_equipsback.jugadors_id = jugadorsback.persones_id
-JOIN football_manager.equips AS equipsback ON equipsback.id = jugadors_equipsback.equips_id
-SET jugadorsback.qualitat = jugadorsback.qualitat + 3
-WHERE jugadorsback.posicions_id = 1 AND equipsback.filial_equips_id IS NOT NULL;
+UPDATE jugadors
+JOIN jugadors_equips ON jugadors_equips.jugadors_id = jugadors.persones_id
+JOIN equips ON equips.id = jugadors_equips.equips_id
+SET jugadors.qualitat = jugadors.qualitat + 3
+WHERE jugadors.posicions_id = 1 AND equips.filial_equips_id IS NULL;
+COMMIT;
 /*ROLLLBACK;*/
 /*COMMIT;*/
-
-
 
 -- 5ta Consulta Modificat
 /*Afegeix un camp a la taula "Jugadors" per indicar si el jugador està disponible per
@@ -84,10 +86,9 @@ UPDATE football_manager.jugadors AS jugadorsback
 JOIN football_manager.jugadors_equips AS jugadors_equipsback ON jugadors_equipsback.jugadors_id = jugadorsback.persones_id
 SET jugadorsback.jugador_disponible = 1
 WHERE YEAR(jugadors_equipsback.data_fitxatge) = 2024 AND MONTH(jugadors_equipsback.data_fitxatge) BETWEEN 1 AND 3;
+COMMIT;
 /*ROLLBACK;*/
 /*COMMIT;*/
-
-
 
 -- 6ta Consulta Modificat
 /*Estem a punt de crear una nova lliga. Hi participaran 6 equips al llarg de 6 jornades. Realitza els INSERT necessaris.*/
@@ -161,10 +162,9 @@ VALUES (0001, '2025-11-20', @id_lliga),
         (0004, '2026-02-25', @id_lliga),
         (0005, '2026-03-01', @id_lliga),
         (0006, '2026-03-22', @id_lliga);
+COMMIT;
 /*ROLLBACK;*/
 /*COMMIT;*/
-
-
 
 -- 7ma Consulta Modificat
 /*Crea els INSERT necessaris per poder afegir una nova
@@ -184,21 +184,22 @@ INSERT INTO football_manager.partits_gols
 VALUES (@partits1, 1, 12, 0),
 		(@partits1, 2, 67, 0),
         (@partits1, 1, 80, 1);
+COMMIT;
 /*ROLLBACK;*/
 /*COMMIT;*/
-
 
 -- 8ve Consulta Modificat
 /*Crea els INSERT necessaris per poder donar un nou
  equip d'altra a la nostra base de dades, incloent el
  seu estadi, l'entrenador i 2 jugadors.*/
 START TRANSACTION;
-
 INSERT INTO football_manager.ciutats (nom)
 VALUES ('Granada');
+SET @ciutat_nova = LAST_INSERT_ID();
 
 INSERT INTO football_manager.estadis (nom, num_espectadors)
 VALUES ('Nortrabeu Estadi', 25000);
+SET @estadi_nou = LAST_INSERT_ID();
 
 INSERT INTO football_manager.equips (nom, any_fundacio, nom_president, ciutats_id, estadis_id)
 VALUES ('Estrella FC', 2012, 'Laura Fernández', @ciutat_nova, @estadi_nou);
@@ -224,17 +225,13 @@ VALUES
 SET @jugador1 = LAST_INSERT_ID() - 1;
 SET @jugador2 = LAST_INSERT_ID();
 
-INSERT INTO football_manager.posicions (posicio)
-VALUES ('Davanter'), ('Migcampista');
-SET @posicio_davanter = LAST_INSERT_ID() - 1;
-SET @posicio_migcampista = LAST_INSERT_ID();
-
 INSERT INTO football_manager.jugadors (persones_id, dorsal, qualitat, posicions_id)
 VALUES
-(@jugador1, 9, 86, @posicio_davanter),
-(@jugador2, 7, 88, @posicio_migcampista);
+(@jugador1, 9, 86, 4),
+(@jugador2, 7, 88, 3);
 
 INSERT INTO football_manager.jugadors_equips (data_fitxatge, jugadors_id, equips_id)
 VALUES
 ('2026-03-21', @jugador1, @equip_nou),
 ('2026-03-21', @jugador2, @equip_nou);
+COMMIT;
